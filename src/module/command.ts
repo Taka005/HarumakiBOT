3b31664f2734feffcdf9a54f46311b03755aa009
Taka005 <01taka005@gmail.com>
2024-07-16 09:20:00 +0000

    change

--- src/module/command.ts
+++ src/module/command.ts
@@ -1,9 +1,18 @@
-import { Client, Routes } from "discord.js";
+import { Client, Routes, SlashCommandBuilder } from "discord.js";
 
 export default async(client: Client)=>{
   if(!client.application) throw new Error("アプリケーションが存在しません");
 
   await client.rest.put(Routes.applicationCommands(client.application.id),{
-    body: []
+    body: [
+      new SlashCommandBuilder()
+        .setName("totalling")
+        .setDescription("募集します")
+        .addIntegerOption(option=>
+          option
+            .setName("number")
+            .setDescription("分ける数")
+            .setRequired(true))
+    ]
   });
 }
--- src/module/event/interactionCreate.ts
+++ src/module/event/interactionCreate.ts
@@ -1,5 +1,47 @@
-import { Interaction } from "discord.js"
+import { Interaction, Colors, ButtonBuilder, ButtonStyle, ActionRowBuilder } from "discord.js"
 
 export default async(interaction: Interaction)=>{
+  if(interaction.isChatInputCommand()){
+    if(interaction.commandName === "totalling"){
+      const number = interaction.options.getInteger("number");
 
+      await interaction.reply({
+        embeds:[{
+          color: Colors.Green,
+          title: `${number}人ずつ分けます`,
+          footer:{
+            text: "0人が募集済み"
+          }
+        }],
+        components:[
+          new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+              new ButtonBuilder()
+                .setCustomId(`totalling_join_${interaction.user.id}`)
+                .setStyle(ButtonStyle.Primary)
+                .setLabel("参加"),
+              new ButtonBuilder()
+                .setCustomId(`totalling_leave_${interaction.user.id}`)
+                .setStyle(ButtonStyle.Danger)
+                .setLabel("脱退"),
+              new ButtonBuilder()
+                .setCustomId(`totalling_stop_${interaction.user.id}`)
+                .setStyle(ButtonStyle.Secondary)
+                .setLabel("締切")
+          )
+        ]
+      });
+    }
+  }else if(interaction.isButton()){
+    if(interaction.customId.startsWith("totalling_")){
+      const data = interaction.customId.split("_");
+
+
+
+      await interaction.message.edit({
+        embeds: interaction.message.embeds,
+        components: interaction.message.components
+      }).catch(()=>{});
+    }
+  }
 }
